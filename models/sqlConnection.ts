0863c87a1e59710b7a8c47a7b1069fff5bc4641a
Administrator <36312455+elicolh@users.noreply.github.com>
2021-12-08 17:10:36 +0100

    mysql pool instead of connection

--- models/sqlConnection.ts
+++ models/sqlConnection.ts
@@ -1,28 +1,35 @@
-
-
 import * as mysql from 'mysql';
+let pool: mysql.Pool;
 
-export let connection:mysql.Connection
 export async function initConnection() {
-    connection = mysql.createConnection({
+    pool = mysql.createPool({
+        connectionLimit: 10,
         host: process.env.MYSQLHOST,
         user: 'jcAnalyzerBot',
-        port:process.env.MYSQLPORT,
+        port: process.env.MYSQLPORT as any,
         password: process.env.MYSQLPASSWORD,
         database: 'jcAnalyzer'
-    });
-    await new Promise<void>((res, rej) => {
-        connection.connect(function (err) {
-            if (err) {
-                rej(err)
+
+    })
+    pool.getConnection((err, connection) => {
+        if (err) {
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                console.error('Database connection was closed.')
             }
-            res()
-        });
+            if (err.code === 'ER_CON_COUNT_ERROR') {
+                console.error('Database has too many connections.')
+            }
+            if (err.code === 'ECONNREFUSED') {
+                console.error('Database connection was refused.')
+            }
+        }
+        if (connection) connection.release()
+        return
     })
 }
-export function query(query: string){
+export function query(query: string) {
     return new Promise((res, rej) => {
-        connection.query(query, (e, result) => {
+        pool.query(query, (e, result) => {
             if (e) {
                 console.error(e)
                 rej(e)
@@ -32,9 +39,9 @@ export function query(query: string){
     }).catch(console.log)
 }
 
-export function getSingleVal(query: string){
+export function getSingleVal(query: string) {
     return new Promise((resolve, rej) => {
-        connection.query(query, (e, res: any) => {
+        pool.query(query, (e, res: any) => {
             if (e) return rej(e)
             // res = Array.from(res)
             if (res.length != 1) return rej("get single prop returned > 1 or 0 row")
@@ -43,9 +50,9 @@ export function getSingleVal(query: string){
         })
     })
 }
-export function getRow(query: string){
+export function getRow(query: string) {
     return new Promise((resolve, rej) => {
-        connection.query(query, (e, res: any) => {
+        pool.query(query, (e, res: any) => {
             if (e) rej(e)
             if (res.length != 1) rej("get single prop returned > 1 or 0 row")
             resolve(res[0])
@@ -53,9 +60,9 @@ export function getRow(query: string){
     })
 }
 
-export function getColumn(query: string){
+export function getColumn(query: string) {
     return new Promise((resolve, rej) => {
-        connection.query(query, (e, res: any) => {
+        pool.query(query, (e, res: any) => {
             if (e) rej(e)
             if (!res.every((e: any) => Object.keys(e).length == 1)) rej("get getColumn returned > 1 or 0 columns")
             resolve(res.map((e: any) => e[Object.keys(e)[0]]))
@@ -63,16 +70,16 @@ export function getColumn(query: string){
     })
 }
 
-export function getMultiple(query: string): Promise < any > {
+export function getMultiple(query: string): Promise<any> {
     return new Promise((resolve, rej) => {
-        connection.query(query, (e, res) => {
+        pool.query(query, (e, res) => {
             if (e) rej(e)
             resolve(res)
         })
     })
 }
 
-export function get(type: string, query: string){
+export function get(type: string, query: string) {
     switch (type) {
         case 'val': return this.getSingleVal(query)
         case 'row': return this.getRow(query)
@@ -82,26 +89,26 @@ export function get(type: string, query: string){
     }
 }
 
-export async function getToken(id:number) {
+export async function getToken(id: number) {
     return await getSingleVal(`SELECT tokenValue FROM tokens where tokenId=${id}`).catch((e) => { debugger })
 }
 
-export function pushTwitterStat(numberofFollowers:number, tweetsWithImpressions:{id:number, impressions:number}[]){
+export function pushTwitterStat(numberofFollowers: number, tweetsWithImpressions: { id: number, impressions: number }[]) {
     query(`INSERT INTO twitterStats (date, numberOfFollowers) VALUES (NOW(), ${numberofFollowers})`)
-        .catch(e=>{throw "error writing into twitterStats"})
-    tweetsWithImpressions.forEach(tweet=>{
+        .catch(e => { throw "error writing into twitterStats" })
+    tweetsWithImpressions.forEach(tweet => {
         query(`INSERT INTO twitterImpressions (tweetId, impressions) VALUES (${tweet.id}, ${tweet.impressions}) ON DUPLICATE KEY UPDATE impressions=${tweet.impressions}`)
     })
 }
-export async function pushInstagramStat(numberOfFollowers: number, currentStories:number){
+export async function pushInstagramStat(numberOfFollowers: number, currentStories: number) {
     query(`INSERT INTO instagramStats (date, numberOfStories, numberOfFollowers) VALUES (NOW(),${currentStories}, ${numberOfFollowers})`)
-        .catch(e=>{throw "error writing into instagramStats"})
+        .catch(e => { throw "error writing into instagramStats" })
 }
 export function pushLinkedinStat(numberofFollowers: number) {
     query(`INSERT INTO linkedinStats (date, numberOfFollowers) VALUES (NOW(), ${numberofFollowers})`)
-        .catch(e=>{throw "error writing into linkedinStats"})
+        .catch(e => { throw "error writing into linkedinStats" })
 }
-export  function pushFacebookStat(numberofFollowers: number) {
-     query(`INSERT INTO facebookStats (date, numberOfFollowers) VALUES (NOW(), ${numberofFollowers})`)
-        .catch(e=>{throw "error writing into facebookStats"})
+export function pushFacebookStat(numberofFollowers: number) {
+    query(`INSERT INTO facebookStats (date, numberOfFollowers) VALUES (NOW(), ${numberofFollowers})`)
+        .catch(e => { throw "error writing into facebookStats" })
 }
