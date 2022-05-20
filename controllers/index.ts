b164939169bae577543bcb5221e7129b58cfa60c
= <admin@example.com>
2022-05-20 15:46:11 +0200

    added total field for most of the stats

--- controllers/index.ts
+++ controllers/index.ts
@@ -2,10 +2,20 @@
  * GET home page.
  */
 import * as express from 'express';
+import { unlinkSync } from "fs"
 const router = express.Router();
 
 router.get('/', (req: express.Request, res: express.Response) => {
-    res.render("index", {host:process.env.HOST})
+    res.render("index", { host: process.env.HOST })
 });
+
+router.put('/forceReload', (req: express.Request, res: express.Response) => {
+    console.log("recu")
+    unlinkSync("cache/facebook.json")
+    unlinkSync("cache/instagram.json")
+    unlinkSync("cache/twitter.json")
+    unlinkSync("cache/linkedin.json")
+    res.send()
+})
 
 export default router;
--- views/public/js/index.js
+++ views/public/js/index.js
@@ -63,7 +63,7 @@ let mychart, data, firstDateFollowers, firstDatePosts, ctx
     });
     //posts details------------- ----------------------
     networkList.forEach(e => {
-        if(e=="linkedin")return
+        if (e == "linkedin") return
         let datesRow = data[e].posts.map(e => e.date)
         let valuesRow = data[e].posts
         datesRow = datesRow.map(e => moment(e))
@@ -145,10 +145,10 @@ let mychart, data, firstDateFollowers, firstDatePosts, ctx
         // datesRow = datesRow.map(e => moment(e))
         let dates = [], values = [], i = 0
         // console.log(veryFirstDate.format("DD/MM/YY"))
-        for (let currWeek = firstDatePosts.clone().day(1); currWeek.isSameOrBefore(moment()); currWeek.add(1, "weeks")) {
-            dates.push(currWeek.valueOf())
+        for (let curr = firstDatePosts.clone().day(1); curr.isSameOrBefore(moment()); curr.add(1, "days")) {
+            dates.push(curr.valueOf())
             let currPostNumber = 0
-            while (i < datesRow.length && datesRow[i].isBefore(currWeek.clone().add(1, "weeks"))) {
+            while (i < datesRow.length && datesRow[i].isBefore(curr.clone().add(1, "days"))) {
                 currPostNumber += 1
                 i++
             }
@@ -156,6 +156,7 @@ let mychart, data, firstDateFollowers, firstDatePosts, ctx
         }
         data[e].posts.values = values
         data[e].posts.dates = dates
+        if (e == "instagram") console.log(dates)
     })
 
     console.log(data)
@@ -182,11 +183,11 @@ $(".mediaSelector").on("click", (e) => {
 })
 $("#dataType").on("change", (e) => {
     // console.log(e.target.value)
-    networkList.forEach(network=>{
-        if(selectableMedias[e.target.value].includes(network)){
-            $("#"+network)[0].style.filter = ""
+    networkList.forEach(network => {
+        if (selectableMedias[e.target.value].includes(network)) {
+            $("#" + network)[0].style.filter = ""
             isSelectable[network] = true
-        }else{
+        } else {
             $("#" + network)[0].style.filter = "grayscale(100%)"
             isSelectable[network] = false
             isVisible[network] = false
@@ -207,6 +208,7 @@ function updateChart() {
     myChart.data.datasets = []
     let everyDates = [];
     if (mode == "follows") {
+        $("#total")[0].className = "d-none"
         myChart.destroy()
         myChart = new Chart(ctx, { ...chartOptions, type: "line" })
         if (myChart.options.scales.y) myChart.options.scales.y.max = undefined
@@ -221,55 +223,72 @@ function updateChart() {
     } else if (mode == "posts") {
         myChart.destroy()
         myChart = new Chart(ctx, { ...chartOptions, type: "bar" })
+        myChart.options.scales.y.suggestedMax = 2
         if (myChart.options.scales.y) myChart.options.scales.y.max = undefined
+        let total = 0
         networkList.forEach(e => {
             // console.log(e)
             if (!isVisible[e]) return
             let { dates, values } = computeData(data[e].posts.dates, data[e].posts.values, debut, fin)
+            total = values.reduce((a, b) => a + b, total)
             everyDates.push(dates)
             myChart.data.datasets.push({ ...defaultDataset, label: e, data: values, backgroundColor: colors[e], borderColor: colors[e], minBarLength: 3 })
         })
+        $("#total")[0].className = ""
+        $("#total")[0].textContent = "Total : " + total
         myChart.data.labels = everyDates[0].map(e => e.format("DD/MM/YYYY"))
-    } else if (mode == "impressions" || mode == "engagement" || mode == "engagementRate" || mode == "likes" || mode == "comments"){
+    } else if (mode == "impressions" || mode == "engagement" || mode == "engagementRate" || mode == "likes" || mode == "comments") {
         myChart.destroy()
         myChart = new Chart(ctx, { ...chartOptions, type: "bar" })
         if (mode == "engagementRate")
             myChart.options.scales.y.max = 1
         else if (myChart.options.scales.y)
             myChart.options.scales.y.max = undefined
+        let total = 0, n = 0
         networkList.forEach(e => {
             // console.log(e)
             if (!isVisible[e]) return
             let dates, values
-            if(e=="linkedin"){
-                if(mode == "engagement"){
-                    ({ dates, values } = computeData(data[e].insights.dates, data[e].insights.values.map(e => e?e.engagementRate * e.impressions:null), debut, fin))
-                }else
-                    ({ dates, values } = computeData(data[e].insights.dates, data[e].insights.values.map(e => e?e[mode]:null), debut, fin))
+            if (e == "linkedin") {
+                if (mode == "engagement") {
+                    ({ dates, values } = computeData(data[e].insights.dates, data[e].insights.values.map(e => e ? e.engagementRate * e.impressions : null), debut, fin))
+                } else
+                    ({ dates, values } = computeData(data[e].insights.dates, data[e].insights.values.map(e => e ? e[mode] : null), debut, fin))
             }
-            else{
-                if(mode == "engagementRate")
-                    ({ dates, values } = computeData(data[e].postsDetails.dates, data[e].postsDetails.values.map(e => e ? (e.likes + e.comments)/e.impressions : null), debut, fin))
-                else if(mode == "engagement")
+            else {
+                if (mode == "engagementRate")
+                    ({ dates, values } = computeData(data[e].postsDetails.dates, data[e].postsDetails.values.map(e => e ? (e.likes + e.comments) / e.impressions : null), debut, fin))
+                else if (mode == "engagement")
                     ({ dates, values } = computeData(data[e].postsDetails.dates, data[e].postsDetails.values.map(e => e ? e.likes + e.comments : null), debut, fin))
                 else
                     ({ dates, values } = computeData(data[e].postsDetails.dates, data[e].postsDetails.values.map(e => e ? e[mode] : null), debut, fin))
             }
+            total = values.reduce((a, b) =>  a + b, total)
+            if (mode == "engagementRate") n+= values.filter(e=>e).length
+            console.log(n)
+            console.log(values.filter(e=>e))
             everyDates.push(dates)
             myChart.data.datasets.push({ ...defaultDataset, label: e, data: values, backgroundColor: colors[e], borderColor: colors[e], minBarLength: 3 })
         })
+        $("#total")[0].className = ""
+            console.log(n)
+        $("#total")[0].textContent = mode == "engagementRate" ? "Moyenne : " + ((total/n)*100).toFixed(1) + "%" :"Total : " + total
         myChart.data.labels = everyDates[0].map(e => e.format("DD/MM/YYYY"))
-    }else if (mode == "stories"){
+    } else if (mode == "stories") {
         myChart.destroy()
         myChart = new Chart(ctx, { ...chartOptions, type: "bar" })
+        let total = 0 
         networkList.forEach(e => {
             // console.log(e)
             if (!isVisible[e]) return
             let { dates, values } = computeData(data[e].storiesHistory.dates, data[e].storiesHistory.values, debut, fin)
             everyDates.push(dates)
             myChart.data.datasets.push({ ...defaultDataset, label: e, data: values, backgroundColor: colors[e], borderColor: colors[e], minBarLength: 3 })
+            total = values.reduce((a,b)=>a+b, total)
         })
         myChart.data.labels = everyDates[0].map(e => e.format("DD/MM/YYYY"))
+        $("#total")[0].className = ""
+        $("#total")[0].textContent = "Total : " + total
     }
     myChart.update()
 }
